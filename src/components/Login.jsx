8c4793ff1b8b6d06be8b38e89ea25c7ae06c8540
paschal533 <okwuosahpaschal@gmail.com>
2022-11-05 11:34:16 +0530

    update commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
 
           <div className="shadow-2xl">
             <GoogleLogin
-              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+              clientId={`700582890406-7blseocj4gorrkktkvf1uem6mhbdt66s.apps.googleusercontent.com`}
               render={(renderProps) => (
                 <button
                   type="button"
